8cd3152dfbbb4e2f7a4d525132b06cc723e91417
Charles Sharp <25charlessharp@usd266.com>
2023-02-13 11:51:24 -0600

    done with reverseCharacters

--- StringLoops.java
+++ StringLoops.java
@@ -1,9 +1,23 @@
-public class Loops {
+public class StringLoops {
 
   // static methods go here
+  public static String reverseCharacters(String str){
+    int index = str.length();
+    String newWord = "";
+    for(index = str.length(); index >= 0; index --){
+      if(index == str.length()){
+        newWord += str.substring(index);
+      }
+      else{
+        newWord += str.substring(index, index +1);
+      }
+    }
+    
+    return newWord;
 
+  }
   public static void main(String[] args) {
-
+    System.out.println(reverseCharacters("radar"));
     // testing of static methods goes here
 
   }
