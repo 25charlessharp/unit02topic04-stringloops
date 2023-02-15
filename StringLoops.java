780aca792c3976fd82ca75e8d063515aff3d4561
Charles Sharp <25charlessharp@usd266.com>
2023-02-15 12:01:27 -0600

    done with reverse

--- StringLoops.java
+++ StringLoops.java
@@ -38,8 +38,33 @@ public class StringLoops {
     return value;
   }
 
+
+  public static boolean hasRepeatedConsectutives(String str){
+    boolean value = false;
+    int index = 0;
+    for(index = 0; index < str.length()-1; index ++){
+      if(str.substring(index, index + 1).equals(str.substring(index + 1, index + 2))){
+        value = true;
+      }
+    }
+    return value;
+  }
+  
+  public static String reverseWords(String str){
+    String str2 = " " + str + " ";
+    String backword = "";
+    int index = 0;
+    int start = 0;
+    for(index = 0; index < str2.length(); index++){
+      if(str2.substring(index, index + 1).equals(" ")){
+        backword = str2.substring(start, index) + ""+ backword;
+        start = index;
+      }
+    }
+    return backword;
+  }
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(indexesOfAll("attack", "b")));
+    System.out.println(reverseWords("the quick brown fox"));
     // testing of static methods goes here
 
   }
